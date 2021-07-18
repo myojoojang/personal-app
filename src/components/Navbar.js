93bebcb775de91d6f775ed8c4e4979d1060a6f02
myojoojang <mjjang0127@gmail.com>
2021-07-18 13:03:48 +0900

    remove cv link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,10 @@ export class Navbar extends Component {
       <div className='Header'>
         <div className='row mx-2 title'>
           <div className="col-12 col-md-6 text-left">MJ JANG. <span className="bold">Frontend Dev</span></div>
-    
+
 
           <div className='semi-title d-flex text-right d-md-block col-md-6 my-auto'>
-            <span>
-              <a href="https://drive.google.com/file/d/1R0gHXZCK1b5CWeruedfQMQIMNE1TovTJ/view?usp=sharing" target="_blank" rel="noopener noreferrer"><i className="fas fa-address-card"></i></a>
-            </span>
+
 
             <span>
               <a href="https://github.com/myojoojang" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square"></i></a>
@@ -20,11 +18,11 @@ export class Navbar extends Component {
             <span>
               <a href="https://www.linkedin.com/in/myojoo-jang-0127/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
             </span>
-        
-        
+
+
           </div>
         </div>
-       
+
       </div>
     )
   }
